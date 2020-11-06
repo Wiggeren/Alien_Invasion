388910714c43affde9454155c0cea2c96cb9961d
Черенцов Игорь <52678893+Wiggeren@users.noreply.github.com>
2020-11-06 23:42:25 +0300

    uploading a project

---
+++ game_stats.py
@@ -0,0 +1,16 @@
+class GameStats():
+	def __init__(self, ai_settings):
+		self.ai_settings = ai_settings
+		self.reset_stats()
+		
+		#Игра запускается в неактивном состоянии
+		self.game_active = False
+		
+		#Рекорд не должен сбрасываться
+		self.high_score = 0
+		
+	def reset_stats(self):
+		self.ships_left = self.ai_settings.ship_limit
+		self.score = 0
+		self.level = 1
+	
